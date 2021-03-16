990001eb79d0bae8bad2f40969ef97842ed02122
Andrin Rehmann <andrinrehmann@gmail.com>
2021-03-16 11:56:02 +0100

    setup

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import "smelte/src/tailwind.css" ;
+	import Button from "smelte/src/components/Button";
+	import Treeview from "smelte/src/components/Treeview";
+</script>
+
+<main>
+	<h1> Hello World! </h1>
+	<Button>Button</Button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
