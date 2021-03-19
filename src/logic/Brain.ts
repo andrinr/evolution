2a515f388070764e0b3e19631b8069986f940399
Andrin Rehmann <andrinrehmann@gmail.com>
2021-03-19 11:57:49 +0100

    some dimension mismatch for process function, not sure why

--- src/logic/Brain.ts
+++ src/logic/Brain.ts
@@ -1,5 +1,12 @@
 import * as math from 'mathjs';
 
+type activationFunction = (value : number) => number;
+
+interface BrainParams {
+    layerSizes: number[],
+    weights?: math.math<number>,
+    activationFunction?: activationFunction,
+}
 /**
  * 
  */
@@ -9,30 +16,41 @@ export class Brain{
     protected weights : math.matrix<number>;
     protected values : math.matrix<number>;
 
+    protected activationFunction : activationFunction;
+
     /**
-     * Initialize a new brain, assuming fully connected, only 1D Layers
+     * Initialize a new neural network, assuming fully connected 1D Layers
      */
-    constructor(layerSizes : number[], weights? : math.matrix<number>){
+    constructor(params : BrainParams){
 
-        const n = layerSizes.length;
-        const m = math.max(layerSizes);
+        this.layerSizes = params.layerSizes;
+
+        const n = this.layerSizes.length;
+        const m = math.max(this.layerSizes);
+
+        console.log(`sizes: ${n} and ${m}`);
         
-        this.weights = weights ? weights : math.random([n, m, m]);
+        this.weights = params.weights ? params.weights : math.random([n, m, m]);
+        this.activationFunction = params.activationFunction ? params.activationFunction : this.logisticFunction;
+    
         this.values = math.zeros([n,m]);
-
-        this.layerSizes = layerSizes;
-
     }
 
-    input(data : math.matrix<number>){
-        const inputIndex = math.index(0,[0,math.size(data)[0]-1])
-        this.values.subset(inputIndex, data);
-        this.propagate();
-        return math.subset(this.values, math.index(-1,[0,this.layerSizes[-1]]));
+    logisticFunction : activationFunction = (x: number) =>{
+        return 1 / ( 1 + Math.exp(-x));
+    }
+
+    process(input : math.matrix<number>){
+        const index = math.index(0,[0,math.size(input)[0]]);
+        console.log(math.size(input)[0]-1);
+        console.log(math.subset(this.values, index));
+        math.subset(this.values, index, input);
+        /*this.propagate();
+        return math.subset(this.values, math.index(-1,[0,this.layerSizes[-1]]));*/
     }
  
     propagate(){
-
+        
     }
 
     mutate(strength : number){
@@ -40,9 +58,6 @@ export class Brain{
     }
 
     clone(){
-        const brainClone = new Brain([...this.layerSizes], this.weights.clone());
-
-        // TODO: copy all weights
-
+        return new Brain({layerSizes : [...this.layerSizes], weights : this.weights.clone()});
     }
 }
